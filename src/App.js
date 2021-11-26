d7bd0fd42f591a5d0caac5dab0239586602244e3
Jort Broekhof <jortdus@gmail.com>
2021-11-26 13:30:22 +0100

    First code of React/ThreeJS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import * as THREE from "three";
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from '@react-three/fiber'
+import { softShadows, MeshWobbleMaterial, OrbitControls, Text } from "@react-three/drei";
+import Header from "./components/header";
+import { useSpring, a } from "@react-spring/three";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+softShadows()
+
+function Box(props) {
+	// This reference gives us direct access to the THREE.Mesh object
+	const ref = useRef()
+	// Hold state for hovered and clicked events
+	const [hovered, hover] = useState(false)
+	const [clicked, click] = useState(false)
+	// Subscribe this component to the render-loop, rotate the mesh every frame
+	useFrame((state, delta) => {
+		// ref.current.rotation.y += delta / 2
+		// ref.current.rotation.x += delta / 3
+	})
+
+	// Return the view, these are regular Threejs elements expressed in JSX
+	return (
+	<group {...props}>
+		<mesh
+		visible={true}
+		position={[0,0,-0.55]}
+		ref={ref}
+		scale={clicked ? 1.5 : 1}
+		onClick={(event) => click(!clicked)}
+		onPointerOver={(event) => hover(true)}
+		onPointerOut={(event) => hover(false)}>
+		<boxGeometry args={[1, 1, 1]} />
+		<meshStandardMaterial color={hovered ? 'blue' : 'orange'} />
+		
+		</mesh>
+		<Text position={[0, 0, 0]} color="black" anchorX="center" anchorY="middle">text</Text>
+	</group>
+	)
+  }
+
+  const App = () => (
+	<Canvas>
+		<ambientLight />
+		<pointLight position={[10, 10, -10]} />
+		<Box position={[0, -1, 0]} />
+		<OrbitControls></OrbitControls>
+	</Canvas>
+  )
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className='logo'>
+        <span>Corona sewage particles per 100.000 residents</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
